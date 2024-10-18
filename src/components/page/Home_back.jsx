f03c141354fe1774cf31304518e1bc972af01f52
JungWooGyun <jwg8910@naver.com>
2024-10-18 17:57:00 +0900

    .

---
+++ src/components/page/Home_back.jsx
@@ -0,0 +1,43 @@
+// import React, { useState, useRef, useEffect } from 'react';
+
+
+
+// import Layout from "../layout/Layout";
+// import "../../styles/Home_back.scss";
+
+
+
+
+// const Home = () => {
+//   const iframeRef = useRef(null);
+
+//   useEffect(() => {
+//     console.log(iframeRef)
+//   }, []);
+
+
+//   return (
+//     <Layout>
+//       <div className="aside">
+//         <div>TITLE</div>
+//         <div>ASIDE</div>
+//         <div>BOTTOM</div>
+//       </div>
+//       <iframe id="iframe_01"
+//         ref={iframeRef}
+//         src="https://safecity.busan.go.kr/#/"  // iframe의 URL
+//         style={{ width: '100vw', height: '100vh' }}
+        
+//         title="Iframe Example"
+//       ></iframe>
+//     </Layout>
+//   );
+// };
+
+// export default Home;
+
+
+// /*
+// npm install leaflet react-leaflet
+// ;
+// */
