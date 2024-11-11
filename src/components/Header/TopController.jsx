84e2bf489fa6b25ebc9d93c1829d2840dffc2c18
JungWooGyun <jwg8910@naver.com>
2024-11-11 17:51:32 +0900

    .

--- src/components/Header/TopController.jsx
+++ src/components/Header/TopController.jsx
@@ -7,7 +7,6 @@ import GaugeChart from "./GaugeChart"
 
 import proj4 from 'proj4';
 
-
 const TopController = ({ CCTV01State, CCTV02State, setCCTV01State, setCCTV02State, floodRiskInfo,map, setFldm_30 })=>{
     const [activeMenu,setActiveMenu] = useState();
     const [activeSubMenu,setActiveSubMenu] = useState();
@@ -89,87 +88,97 @@ const TopController = ({ CCTV01State, CCTV02State, setCCTV01State, setCCTV02Stat
 
 
     // WMS 적용!!
+    
+    // Proj4 설정
+    proj4.defs("EPSG:5181", "+proj=tmerc +lat_0=38 +lon_0=127.5 +k=1 +x_0=200000 +y_0=600000 +ellps=GRS80 +units=m +no_defs");
+
     const Fldm_30Handler = (e) => {
-    
-       
-        //https://safecity.busan.go.kr/geoserver/iots/wms?service=WMS&request=GetCapabilities
+
+        
+        const mapContainer = document.getElementById('map'); // HTML 요소 id로 가져오기
+        const width = mapContainer.offsetWidth;  // 가로 크기
+        const height = mapContainer.offsetHeight; // 세로 크기
+
+        console.log("지도 가로 크기 (px):", width);
+        console.log("지도 세로 크기 (px):", height);
 
 
-        setFldm_30State(!fldm_30State);
+        const div = document.createElement('div');
+        
+        const url = `https://safecity.busan.go.kr/geoserver/iots/wms?service=WMS&request=GetMap&layers=fldm_30&styles=&format=image%2Fpng&transparent=true&version=1.1.1&width=${width}&height=${height}&srs=CRS%3A84&bbox=128.8048024262355,35.00381579648614,129.28960715493199,35.38509155161659`;
+        
 
-        if (!fldm_30State) {
-            // 타일셋 정의 및 추가
-            window.kakao.maps.Tileset.add('TILE_NUMBER', 
-                new window.kakao.maps.Tileset({
+        var content = `<div class ="label" style="">TEST</span></div>`;
+        var position = new window.kakao.maps.LatLng(35.17944, 129.07556);  
+        // 커스텀 오버레이를 생성합니다
+        var customOverlay = new window.kakao.maps.CustomOverlay({
+            position: position,
+            content: content   
+        });
+
+        // 커스텀 오버레이를 지도에 표시합니다
+        customOverlay.setMap(map);
+
+        //-----------------------------------
+        // 타일형으로 부여하기(실패)
+        //-----------------------------------
+        // //https://safecity.busan.go.kr/geoserver/iots/wms?service=WMS&request=GetCapabilities
+
+
+        // setFldm_30State(!fldm_30State);
+
+
+        // let i_x = 0;
+        // let i_y = 0;
+
+        // if (!fldm_30State) {
+        //      // 타일셋 정의 및 추가
+        //     window.kakao.maps.Tileset.add('TILE_NUMBER', 
+        //         new window.kakao.maps.Tileset({
                     
-                    width: 256,
-                    height: 256,
-                   
-                    getTile: function (x, y, z) {
-                        console.log(x, y, z);
-    
-                        const tileSize = 256; // 타일 크기
-                        
-                        // 기준 좌표 설정 (지도 왼쪽 상단의 좌표)
-                        const originX = 169896.25; // 최소 X 값
-                        const originY = 408063.0; // 최대 Y 값, 위에서 아래로 좌표가 감소한다고 가정
+        //             width: 256,
+        //             height:256,
+                
+        //             getTile: function (x, y, z) {
+        //                 const tileSize = 256;
 
 
-                        // 줌 레벨에 따른 스케일 계산 (EPSG:5181 기준 스케일)
-                        const scale = Math.pow(2, 18 - z); // 18은 기본 줌 레벨 기준
-                        
-                        // 타일 크기에 스케일을 곱하여 현재 줌 레벨의 타일 크기 계산
-                        const scaledTileSize = tileSize * scale;
+        //                 // 타일 생성 및 스타일 설정
+        //                 const div = document.createElement('div');
 
-                        // 타일의 좌표를 기준으로 `bbox` 계산
-                        let minX = originX + (x * scaledTileSize);
-                        let maxY = originY - (y * scaledTileSize); // Y 방향은 위에서 아래로 감소
-                        let maxX = minX + scaledTileSize;
-                        let minY = maxY - scaledTileSize;
-                        minX="169896.25" ;
-                        maxY="364757.59375" ;
-                        maxX="211255.65625" ;
-                        minY="408063.0"
-                        // bbox 계산
-                        const bbox = `${minX},${minY},${maxX},${maxY}`;
+        //                 // const url = `https://safecity.busan.go.kr/geoserver/iots/wms?service=WMS&request=GetMap&layers=fldm_30&styles=&format=image%2Fpng&transparent=true&version=1.1.1&width=${width}&height=${height}&srs=CRS%3A84&bbox=128.8048024262355,35.00381579648614,129.28960715493199,35.38509155161659`;
+        //                 // div.style.backgroundImage = `url(${url})`;
+                      
+        //                 div.style.backgroundSize = 'cover';
+        //                 div.style.width = '100%';
+        //                 div.style.height = '100%';
+        //                 div.style.opacity = '1';
+        //                 div.style.fontSize = '2.5rem';
+        //                 div.style.border = `1px solid black`;
+                        
+        //                 // 타일 좌표 정보를 텍스트로 표시
+        //                 div.innerHTML = `${x} ${y} ${z}`;
+        //                 div.style.color = 'black';
+        //                 div.style.display = 'flex';
+        //                 div.style.alignItems = 'center';
+        //                 div.style.justifyContent = 'center';
+        //                 div.style.position = 'relative';
+        //                 div.style.boxSizing = 'border-box';
 
-                        
-                        console.log(bbox);
+        //                 div.innerHTML+=`<button style='z-index:99999;width:25px;height:25px;position:absolute;left:0;top:0;font-size:.3rem;cursor:pointer' >버튼</button>`
 
-                        // WMS 요청 URL 생성
+        //                 return div;
+        //             }
 
-                        const wmsUrl = `https://safecity.busan.go.kr/geoserver/iots/wms?service=WMS&request=GetMap&layers=fldm_30&styles=&format=image/png&transparent=true&version=1.1.1&width=${tileSize}&height=${tileSize}&srs=EPSG:5181&bbox=${bbox}`;
-                        
-                        // 타일 생성 및 스타일 설정
-                        const div = document.createElement('div');
-                        div.style.backgroundImage = `url(${wmsUrl})`;
-                        div.style.backgroundSize = 'cover';
-                        div.style.width = '100%';
-                        div.style.height = '100%';
-                        div.style.opacity = '1';
-                        div.style.fontSize = '2.5rem';
-                        
-                        div.style.border = `1px solid black`;
-                        
-                        // 타일 좌표 정보를 텍스트로 표시
-                        div.innerHTML = `${x} ${y} ${z}`;
-                        div.style.color = 'black';
-                        div.style.display = 'flex';
-                        div.style.alignItems = 'center';
-                        div.style.justifyContent = 'center';
-                        
-                        return div;
-                    }
-        
-                })
-            );
-        
-            // 타일 오버레이 추가
-            map.addOverlayMapTypeId(window.kakao.maps.MapTypeId.TILE_NUMBER);
-        } else {
-            // 타일 오버레이 제거
-            map.removeOverlayMapTypeId(window.kakao.maps.MapTypeId.TILE_NUMBER);
-        }
+        //         })
+        //     );
+
+        //     // 타일 오버레이 추가
+        //     map.addOverlayMapTypeId(window.kakao.maps.MapTypeId.TILE_NUMBER);
+        // } else {
+        //     // 타일 오버레이 제거
+        //     map.removeOverlayMapTypeId(window.kakao.maps.MapTypeId.TILE_NUMBER);
+        // }
 
         
 
