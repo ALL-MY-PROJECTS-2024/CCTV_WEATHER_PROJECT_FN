2d52f3b4f1892a71a6d33bfb1ba9cc90b00d6068
JungWooGyun <jwg8910@naver.com>
2024-12-13 16:32:30 +0900

    .

--- src/components/FloodingPopupTEST.jsx
+++ src/components/FloodingPopupTEST.jsx
@@ -41,20 +41,32 @@ const FloodingPopupTEST = ({ hlsAddr, setSelectedFLOODING,instl_pos }) => {
   };
 
   return (
+
+
     <div className="flooding-popup-overlay" 
-      style={{backgroundColor:"",position:"relative",left:'0',top:"70px",width:"100%",height:"80%",overflow:"hidden"}}
+      style={{backgroundColor:"",position:"relative",left:'0',top:"70px",width:"100%",minWidth:"300px",height:"80%",minHeight:"200px",overflow:"hidden"}}
     >
-      <div className="flooding-popup" style={{height:"100%",width:"100%",}}>
+
+        <div style={{position:"absolute",left:"10px",height:"30px",top:"5px",width:"50px",zIndex:"999"}}>
+                <div style={{display:"flex",width:"80px",color:"white",alignItems:"center",height:"100%"}}>
+                    <span class="material-symbols-outlined" style={{fontSize:"1.5rem",color:"#40FF00"}}>notifications</span>
+                    <span style={{fontSize:".8rem"}}>안전</span>    
+                </div>
+
+                
+        </div>
+              
+      <div className="flooding-popup" style={{height:"100%",width:"100%",position:"relative",top:"20px"}}>
 
         <div className="flooding-items"  style={{display:"flex",alignItems:"center",justifyContent:"center",height:"100%",width:"100%",position:"relative",top:"0px" ,margin:"0"}} >
-          <div className="grid-item "  style={{height:"100%",width:"100%" ,border:"1px solid white"}}>
+          <div className="grid-item "  style={{height:"100%",width:"100%" ,}}>
             
             <div className="title" style={{position:"absolute",left:"0",top:"0", height:"30px",width:"100%",backgroundColor:"transparent",color:""}} >
-              <span style={{position:"absolute",left:"0",top:"0",zIndex:"999",fontSize:"2rem",color:"white"}}>제목</span>
+              <span style={{position:"absolute",left:"5px",top:"5px",zIndex:"999",fontSize:"1.5rem",color:"white"}}>제목</span>
               <button
                 onClick={closeHandler}
                 className="close-btn"
-                style={{zIndex:"999",backgroundColor:"",fontSize:"1rem",position:"absolute",right:'0',top:"0",width:"30px",height:"30px"}}
+                style={{zIndex:"999",backgroundColor:"",fontSize:"1rem",position:"absolute",right:'5px',top:"5px",width:"30px",height:"30px"}}
               >
                 X
               </button>
@@ -81,6 +93,7 @@ const FloodingPopupTEST = ({ hlsAddr, setSelectedFLOODING,instl_pos }) => {
         </div>
       </div>
     </div>
+
   );
 };
 
--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -2,20 +2,27 @@ import React, { useEffect, useState, useCallback } from 'react';
 import { union } from "@turf/turf"; // 기본 내보내기가 아닌 union 모듈만 임포트
 import * as turf from "@turf/turf";
 
+import {Link} from 'react-router-dom'
 
 // COMPONENT
 import FloodingPopupTEST from "./FloodingPopupTEST";
 import ShowMap from "./ShowMap"
+import Weather from "./Weather"
+import GaugeChart from "../components/Header/GaugeChart"
 
 //Dataset
 import busanGeoJson from '../dataSet/busan.json';
 import floodingData from '../dataSet/FLOODING.json';
 
 
+import axios from 'axios';
 
 
-//test
+
+//DATASET_TEST
 import JSON_TEST from '../dataSet/침수위험 수치모델 이미지 데이터/내수/동래구/030yr_060/Dongnae_030_1_00019.json'
+
+
 
 import "./Test.scss";
 
@@ -29,7 +36,9 @@ import "proj4"
 import "proj4leaflet"
 import L, { CRS, bounds } from 'leaflet';
 
-// Throttle function to limit the frequency of calls
+//------------------------------------------------------------------------
+// 쓰로틀링
+//------------------------------------------------------------------------
 const throttle = (func, delay) => {
     let lastCall = 0;
     return (...args) => {
@@ -39,7 +48,158 @@ const throttle = (func, delay) => {
         return func(...args);
       }
     };
-  };
+};
+
+//------------------------------------------------------------------------
+// 날씨 정보 가져오기 - 초단기 실황조회
+//------------------------------------------------------------------------
+const fetchWeatherInfo_Ultra = async (latitude, longitude) => {
+  // 현재 날짜와 시간을 가져와서 포맷
+  const now = new Date();
+
+  // 가능한 base_time 목록 (30분 단위로 설정)
+  const availableTimes = [
+    "0200",
+    "0500",
+    "0800",
+    "1100",
+    "1400",
+    "1700",
+    "2000",
+    "2300",
+  ];
+
+  // 현재 시간에서 가능한 base_time을 찾음
+  let baseTime = "";
+  for (let i = availableTimes.length - 1; i >= 0; i--) {
+    const [hour, minute] = [parseInt(availableTimes[i].slice(0, 2)), 0];
+    const baseDateTime = new Date(
+      now.getFullYear(),
+      now.getMonth(),
+      now.getDate(),
+      hour,
+      minute
+    );
+
+    // 현재 시간보다 이전의 base_time을 찾음
+    if (now >= baseDateTime) {
+      baseTime = availableTimes[i];
+      break;
+    }
+  }
+
+  // 자정 이전의 base_time을 사용하는 경우 하루 전 날짜로 설정
+  let baseDate = now;
+  if (now.getHours() < 8) {
+    baseDate.setDate(now.getDate() - 1);
+    baseTime = "2300";
+  }
+
+  // base_date 포맷: YYYYMMDD
+  const formattedDate = `${baseDate.getFullYear()}${String(
+    baseDate.getMonth() + 1
+  ).padStart(2, "0")}${String(baseDate.getDate()).padStart(2, "0")}`;
+
+  const gridCoords = dfs_xy_conv("toXY", latitude, longitude);
+  console.log(
+    `baseDate : ${formattedDate} base_time : ${baseTime} 격자 좌표: x=${gridCoords.x}, y=${gridCoords.y}`
+  );
+
+  try {
+    const resp = await axios.get(
+      `http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst`,
+      {
+        params: {
+          ServiceKey:
+            "xYZ80mMcU8S57mCCY/q8sRsk7o7G8NtnfnK7mVEuVxdtozrl0skuhvNf34epviHrru/jiRQ41FokE9H4lK0Hhg==",
+          pageNo: 1,
+          numOfRows: 450,
+          dataType: "JSON",
+          base_date: formattedDate,
+          base_time: baseTime,
+          nx: gridCoords.x,
+          ny: gridCoords.y,
+        },
+      }
+    );
+    return resp;
+  } catch (error) {
+    console.error("날씨 정보를 가져오는 데 실패했습니다:", error);
+    return null;
+  }
+};
+//날씨정보
+function dfs_xy_conv(code, v1, v2) {
+  //<!--
+  //
+  // LCC DFS 좌표변환을 위한 기초 자료
+  //
+  var RE = 6371.00877; // 지구 반경(km)
+  var GRID = 5.0; // 격자 간격(km)
+  var SLAT1 = 30.0; // 투영 위도1(degree)
+  var SLAT2 = 60.0; // 투영 위도2(degree)
+  var OLON = 126.0; // 기준점 경도(degree)
+  var OLAT = 38.0; // 기준점 위도(degree)
+  var XO = 43; // 기준점 X좌표(GRID)
+  var YO = 136; // 기1준점 Y좌표(GRID)
+  //
+  // LCC DFS 좌표변환 ( code : "toXY"(위경도->좌표, v1:위도, v2:경도), "toLL"(좌표->위경도,v1:x, v2:y) )
+  //
+
+  var DEGRAD = Math.PI / 180.0;
+  var RADDEG = 180.0 / Math.PI;
+
+  var re = RE / GRID;
+  var slat1 = SLAT1 * DEGRAD;
+  var slat2 = SLAT2 * DEGRAD;
+  var olon = OLON * DEGRAD;
+  var olat = OLAT * DEGRAD;
+
+  var sn =
+    Math.tan(Math.PI * 0.25 + slat2 * 0.5) /
+    Math.tan(Math.PI * 0.25 + slat1 * 0.5);
+  sn = Math.log(Math.cos(slat1) / Math.cos(slat2)) / Math.log(sn);
+  var sf = Math.tan(Math.PI * 0.25 + slat1 * 0.5);
+  sf = (Math.pow(sf, sn) * Math.cos(slat1)) / sn;
+  var ro = Math.tan(Math.PI * 0.25 + olat * 0.5);
+  ro = (re * sf) / Math.pow(ro, sn);
+  var rs = {};
+  if (code == "toXY") {
+    rs["lat"] = v1;
+    rs["lng"] = v2;
+    var ra = Math.tan(Math.PI * 0.25 + v1 * DEGRAD * 0.5);
+    ra = (re * sf) / Math.pow(ra, sn);
+    var theta = v2 * DEGRAD - olon;
+    if (theta > Math.PI) theta -= 2.0 * Math.PI;
+    if (theta < -Math.PI) theta += 2.0 * Math.PI;
+    theta *= sn;
+    rs["x"] = Math.floor(ra * Math.sin(theta) + XO + 0.5);
+    rs["y"] = Math.floor(ro - ra * Math.cos(theta) + YO + 0.5);
+  } else {
+    rs["x"] = v1;
+    rs["y"] = v2;
+    var xn = v1 - XO;
+    var yn = ro - v2 + YO;
+    ra = Math.sqrt(xn * xn + yn * yn);
+    if (sn < 0.0) ra = -ra;
+    var alat = Math.pow((re * sf) / ra, 1.0 / sn);
+    alat = 2.0 * Math.atan(alat) - Math.PI * 0.5;
+
+    if (Math.abs(xn) <= 0.0) {
+      theta = 0.0;
+    } else {
+      if (Math.abs(yn) <= 0.0) {
+        theta = Math.PI * 0.5;
+        if (xn < 0.0) theta = -theta;
+      } else theta = Math.atan2(xn, yn);
+    }
+    var alon = theta / sn + olon;
+    rs["lat"] = alat * RADDEG;
+    rs["lng"] = alon * RADDEG;
+  }
+  return rs;
+}
+
 
 
 //------------------------------------------------------------------------
@@ -78,8 +238,12 @@ const Test  = ()=>{
     //
     const [도시지역 , set도시지역] = useState(["북구", "중구", "동래구", "사상구"])
 
+    //법정동 지도
+    const [selectedMapLayer , setSelectedMapLayer] = useState(null)
+    const [selectedFloodingMapLayer , setselectedFloodingMapLayer] = useState(null)
+
     //침수 
-    const [floodingState, setFloodingState] = useState(true);
+    const [floodingState, setFloodingState] = useState(false);
     const [clusterFLOODING, setClusterFLOODING] = useState([]);
     const [selectedFLOODING, setSelectedFLOODING] = useState(null);  // 선택된 CCTV 정보 상태 추가
 
@@ -88,6 +252,25 @@ const Test  = ()=>{
    
     //지도
     const [selectedShowMap, setSelectedShowMap] = useState(null)
+    //날씨
+    const [weatherData,setWeatherData] = useState(null)
+    //
+    const [floodRiskInfo, setFloodRiskInfo] = useState(null); 
+
+    //----------------------------
+     // Fetch flood risk info
+     //----------------------------
+     const fetchFloodRiskInfo = async (latitude, longitude) => {
+      try {
+        const response = await axios.get(`https://safecity.busan.go.kr/iots/base/gisAlarm.do?latitude=${latitude}&longitude=${longitude}`);
+        setFloodRiskInfo(response.data);
+        console.log("Flood Risk Info:", response.data);
+      } catch (error) {
+        console.error("Failed to fetch flood risk info:", error);
+        setFloodRiskInfo({ Dong: "서비스 지역을 벗어났습니다." ,info:""});
+      }
+    };
+    const throttledFetchFloodRiskInfo = useCallback(throttle(fetchFloodRiskInfo, 300), []);
 
     //----------------------------
     //
@@ -143,22 +326,56 @@ const Test  = ()=>{
 
             // 지도 이동 시 이벤트
             mapInstance.on('moveend', () => {
-            const center = mapInstance.getCenter();
-            setCenterPosition({ lat: center.lat, lng: center.lng });
-            
+              const center = mapInstance.getCenter();
+              setCenterPosition({ lat: center.lat, lng: center.lng });
+              throttledFetchFloodRiskInfo(center.lat, center.lng);
             });
 
         }
 
       }, []);
 
+
+
+   //-----------------------------------
+   // 구역선택시(침수구역제외)
+   //-----------------------------------
+   useEffect(()=>{
+    
+    console.log("selectedMapLayer....!!",selectedMapLayer)
+    const req =  async ()=>{
+      try {
+       
+         const resp = await fetchWeatherInfo_Ultra(selectedMapLayer.lat , selectedMapLayer.lng)
+         const data = resp.data
+         console.log("data",data)
+
+
+      } catch (error) {
+        console.error("날씨 정보를 가져오는 데 실패했습니다:", error);
+        return null;
+      }
+    }
+    req();
+
+
+   },[selectedMapLayer])
+
+
+   //-----------------------------------
+   // 침수 구역선택시
    //-----------------------------------
    //
+   //
+   //
+
+   //-----------------------------------
+   // 침수 마커
    //-----------------------------------
    useEffect(() => {
     
     const fetchCCTVData = async () => {
-      console.log("fetchCCTVData invoked...")
+      
       try {
         setClusterFLOODING(floodingData.map(item => ({ ...item, type: 'FLOODING' })));
       } catch (error) {
@@ -197,7 +414,11 @@ const Test  = ()=>{
 
         clusterFLOODING.forEach((marker) => {
           L.marker([marker.lat, marker.lon], { icon: createFLOODINGMarkerIcon() })
-            .on('click', () => setSelectedFLOODING(marker))
+            .on('click', () => {
+                //
+                setSelectedFLOODING(marker)//
+              
+              })
             .addTo(clusterGroup3);
         });
         map.addLayer(clusterGroup3);
@@ -206,7 +427,7 @@ const Test  = ()=>{
           map.removeLayer(clusterGroup3);
         };
       }
-    }, [map, floodingState, clusterFLOODING]);
+    }, [map, floodingState, clusterFLOODING,selectedFLOODING]);
 
 
 
@@ -281,8 +502,15 @@ const Test  = ()=>{
             .on("click", function (e) {
                
                
-                
+                //
+
+
                 const names =  e.layer.feature.properties.adm_nm.split(" ")
+                const bounds = e.layer.getBounds();
+                const center = bounds.getCenter();
+               
+                setSelectedMapLayer(center)
+
                 //console.log(names[1])
                 if(도시지역.includes(names[1])){
                   console.log('clicked..',names[1])
@@ -305,7 +533,7 @@ const Test  = ()=>{
             .addTo(map)
                 
         }
-    },[map])
+    },[map,selectedMapLayer])
 
 
 
@@ -330,7 +558,11 @@ const Test  = ()=>{
               <li>메뉴3</li>  
 
 
-              <li className="etc">기타메뉴</li>  
+              <li className="etc">
+                      <Link to="">
+                        <span class="material-symbols-outlined">settings</span>
+                      </Link>
+              </li>  
             </ul>
           </nav>
  
@@ -340,14 +572,16 @@ const Test  = ()=>{
 
           <div className="left">
             <div className="map-item">
+              <div className="gaugeChart-block">
+                <GaugeChart level={95} floodRiskInfo={floodRiskInfo} />
+              </div>
+              
               <div id="map" style={{  backgroundColor:"#393F4F",objectFit:"contain"}}></div>
               
 
               {selectedShowMap&&<ShowMap />
               
               }
-              
-
 
             </div>
             <div className="map-control-item">
@@ -373,22 +607,52 @@ const Test  = ()=>{
                 </a>
               </div>
               <div className="item">
-                <a href="javascript:void(0)">
-                <span class="material-symbols-outlined">package_2</span>
+
+                <a href="javascript:void(0)" onClick={e=>{setFloodingState(!floodingState)}}>
+                  {floodingState ? (
+                    <span class="material-symbols-outlined" style={{color:"royalblue"}}>package_2</span>
+                  ):(
+                    <span class="material-symbols-outlined" >package_2</span>
+                  )
+                }
                 </a>
               </div>
 
             </div>  
             <div className="weather-item">
-                <div className="item"></div>
-                <div className="item"></div>
-                <div className="item"></div>
-                <div className="item"></div>
+              
+                <div className="item">
+                  <div>
+                    <span class="material-symbols-outlined" style={{color:"#40FF00"}}>thermometer</span>
+                    <div style={{textAlign:"center",color:"white"}}><span>23.5</span> ℃</div>
+                  </div>
+                </div>
+                <div className="item">
+                  <div>
+                    <span class="material-symbols-outlined" style={{color:"#FFFF00"}}>sunny</span>
+                    <div style={{textAlign:"center",color:"white"}}><span></span></div>
+                  </div>
+                </div>
+                <div className="item">
+                  <div>
+                    <span class="material-symbols-outlined" style={{color:"#40FF00"}}>thermometer</span>
+                    <div style={{textAlign:"center",color:"white"}}><span>23.5</span> ℃</div>
+                  </div>
+                </div>
+                <div className="item">
+                  <div>
+                    <span class="material-symbols-outlined" style={{color:"#40FF00"}}>thermometer</span>
+                    <div style={{textAlign:"center",color:"white"}}><span>23.5</span> ℃</div>
+                  </div>
+                </div>
+
             </div>
           </div>
           <div className="right">
+     
               <div className="videoBlock" style={{overflow:"hidden"}}>
-                {selectedFLOODING && (
+                
+                {selectedFLOODING ? (
                         <FloodingPopupTEST 
                           lat={selectedFLOODING.lat}
                           lon={selectedFLOODING.lon}
@@ -396,6 +660,12 @@ const Test  = ()=>{
                           instl_pos={selectedFLOODING.instl_pos}
                           setSelectedFLOODING={setSelectedFLOODING}
                         />
+                  ) : (
+                    <>
+                      <div style={{color:"white"}}>
+                        TEST
+                      </div>
+                    </>
                   )}
 
               </div>
